e34042efc5ca838879cfc50ca2a86b8ed4b42763
Ana Varani <apvarani@gmail.com>
2022-05-17 18:39:23 +0100

    exclude FloatingWindow type from set title

--- app/src/main/java/com/varani/gtmotors/MainActivity.kt
+++ app/src/main/java/com/varani/gtmotors/MainActivity.kt
@@ -2,6 +2,7 @@ package com.varani.gtmotors
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.FloatingWindow
 import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
@@ -16,8 +17,10 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
-        navController.addOnDestinationChangedListener { _, _, _ ->
-            supportActionBar?.title = navController.currentDestination?.label
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination !is FloatingWindow) {
+                supportActionBar?.title = destination.label
+            }
         }
     }
 }
